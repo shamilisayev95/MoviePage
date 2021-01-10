c363e8ce76c4f882c4a945e0c4b912783a9b4241
shamilisayev95 <shamil.isayev95@gmail.com>
2021-01-10 16:12:10 +0400

    ready

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,34 +1,26 @@
 import React from 'react';
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import { Carousel } from 'react-responsive-carousel';
-
+import "./home.css";
 
 function Home() {
     return (
-        <div className='root__main-container'>
-          <header className='main-container__header'>
-            WELCOME TO Homepage
-          </header>
-          <main className='main__container'>
-            <div>
-            <Carousel>
-                <div>
-                    <img src="" />
-                    <p className="legend">Movie 1</p>
-                </div>
-                <div>
-                    <img src="https://news.artnet.com/app/news-upload/2016/03/6358055488201275001050560_42_DGA_20151015_PB_0594.jpg" />
-                    <p className="legend">Movie 2</p>
-                </div>
-                <div>
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQT2EkKybcPBeBjxjUKKJl2s94_ZePPKRjUQ&usqp=CAU" />
-                    <p className="legend">Movie 3</p>
-                </div>
-            </Carousel>
+        <div className={"root__container-home"}>
+          <div className={"wrapper"}>
+            <input type="radio" name="point" id="slide1" defaultChecked />
+            <input type="radio" name="point" id="slide2" />
+            <input type="radio" name="point" id="slide3" />
+            <div className={"slider"}>
+              <div className={"slides slide1"}></div>
+              <div className={"slides slide2"}></div>
+              <div className={"slides slide3"}></div>
             </div>
-          </main>
+            <div className={"controls"}>
+              <label htmlFor="slide1"></label>
+              <label htmlFor="slide2"></label>
+              <label htmlFor="slide3"></label>
+            </div>
+          </div>
         </div>
-    );
+      );
   }
 
 export default Home;
